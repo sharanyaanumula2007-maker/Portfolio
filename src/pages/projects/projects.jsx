0b8f5f232276143b51ed12d6c458a0044c76329f
Sharanya <sharanyaanumula2007@gmail.com>
2026-09-10 23:47:07 +0530

    Developed projects webpage

--- src/pages/projects/projects.jsx
+++ src/pages/projects/projects.jsx
@@ -1,15 +1,20 @@
 import { Header } from "../../components/Header";
+import { projects } from "./projectsData";
+import './projects.css';
 
 export function Projects(){
     return(
         <>
             <Header/>
             <div className="projects">
-                <div className="project">
-                    <img src="project-img1" alt="project-img1" />
-                    <h2 className="project-name">Simple Calculator</h2>
-                    
+                {projects.map((project,index)=>(
+                    <div className="project" key={index}>
+                    <img className="project-img" src={project.img} alt={project.name} />
+                    <h2 className="project-name">{project.name}</h2>
+                    <p className="project-description">{project.description}</p>
+                    <a href={project.link} target="_blank" rel="noreferrer" className="project-link">View Project</a>
                 </div>
+                ))}
             </div>
         </>
     );
---
+++ src/pages/projects/projectsData.js
@@ -0,0 +1,29 @@
+import calculatorImg from '../../assets/calculator.webp';
+import toDoImg from '../../assets/to-do.jpg';
+import defaultProjectImg from '../../assets/project.png';
+
+export const projects=[{
+        name:"Portfolio",
+        img:defaultProjectImg,
+        description:"My personal portfolio website showcasing my skills, projects, and certifications, while providing a way to connect with me as an aspiring software developer.",
+        link:"https://github.com/sharanyaanumula2007-maker/Portfolio"
+    },
+    {
+        name:"Simple Calculator",
+        img:calculatorImg,
+        description:"A frontend calculator web application developed using HTML, CSS, and JavaScript that performs basic arithmetic operations including addition, subtraction, multiplication, and division.",
+        link:"https://github.com/sharanyaanumula2007-maker/Simple-Calculator"
+    },
+    {
+        name:"to-do-List-project",
+        img:toDoImg,
+        description:"A frontend-based task management website that enables users to add tasks, mark them as completed, and delete tasks when they are no longer needed.",
+        link:"https://github.com/sharanyaanumula2007-maker/to-do-List-project"
+    },
+    {
+        name:"Simple-quiz",
+        img:defaultProjectImg,
+        description:"A frontend-based quiz application developed using HTML, CSS, and JavaScript that presents multiple-choice questions, evaluates user responses, calculates the score, and displays the final result at the end of the quiz.",
+        link:"https://github.com/sharanyaanumula2007-maker/Simple-quiz"
+    }
+    ];
