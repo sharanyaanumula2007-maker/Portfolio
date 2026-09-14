728ce43e2c4804c5aaba77b510ac463f53ecd78f
Sharanya <sharanyaanumula2007@gmail.com>
2026-09-14 22:46:56 +0530

    created contact page html

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -2,7 +2,46 @@ import { Header } from "../../components/Header";
 
 export function Contact(){
     return(
-        <Header/>
-
+        <>
+            <Header/>
+            <div className="contact-form">
+                <div className="name-fields">
+                    <div className="form-group">
+                        <label htmlFor="firstName">First Name:</label>
+                        <input 
+                            type="text"
+                            id="firstName"
+                            name="firstName"   
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="lastName">Last Name:</label>
+                        <input 
+                            type="text"
+                            id="lastName"
+                            name="lastName"   
+                        />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Email:<span>*</span></label>
+                    <input 
+                        type="email"
+                        id="email"
+                        name="email" 
+                        required  
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="message">Write a message:</label>
+                    <textarea 
+                        id="message"
+                        name="message" 
+                        rows="5"
+                    ></textarea>
+                </div>
+                <button type="submit">Send message</button>
+            </div>
+        </>
     );
 }
