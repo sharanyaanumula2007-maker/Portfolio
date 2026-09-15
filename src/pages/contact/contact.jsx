95445fc44f657500d7d73b5b18422acc0964c022
Sharanya <sharanyaanumula2007@gmail.com>
2026-09-15 22:34:40 +0530

    styled contact page

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -1,10 +1,36 @@
 import { Header } from "../../components/Header";
+import { useState } from "react";
+import './contact.css';
 
 export function Contact(){
+    const [sent,setSent] =useState(false);
+    const handleSubmit =(e) =>{
+        e.preventDefault();
+        const form=e.target;
+        const firstName=form.firstName.value;
+        const lastName=form.lastName.value;
+        const email=form.email.value;
+        const message=form.message.value;
+
+        const subject=`Portfolio Contact from ${firstName} ${lastName}`;
+        const body =`
+        Name:${firstName} ${lastName}
+        Email: ${email}
+        Message:
+        ${message}`;
+        const gmailURL=
+        `https://mail.google.com/mail/?view=cm&fs=1&to=sharanyaanumula2007@gmail.com`+
+        `&su=${encodeURIComponent(subject)}`+
+        `&body=${encodeURIComponent(body)}`;
+        window.open(gmailURL,"_blank");
+        setSent(true);
+        form.reset();
+    };
+
     return(
         <>
             <Header/>
-            <div className="contact-form">
+            <form className="contact-form" onSubmit={handleSubmit}>
                 <div className="name-fields">
                     <div className="form-group">
                         <label htmlFor="firstName">First Name:</label>
@@ -40,8 +66,13 @@ export function Contact(){
                         rows="5"
                     ></textarea>
                 </div>
-                <button type="submit">Send message</button>
-            </div>
+                <button type="submit">Send Message <span className="send-icon">✈</span></button>
+                {sent &&(
+                    <p className="success-message">
+                        Message sent successfully! Thank you for contacting me .💜
+                    </p>
+                )}
+            </form>
         </>
     );
 }
